196b6836f5cf86118549b4e6763394ed0cab233b
gabriel <fenexs@outlook.com>
2019-07-11 20:37:12 -0300

    cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');//backend framework for nodejs
 const mongoose = require('mongoose');//mongodb framework
 const requireDir = require('require-dir');
+const cors = require('cors');
 
 //Iniciando o app
 const app = express();//iniciando o framework
 app.use(express.json());//para o express aceitar post com json
 app.use(express.urlencoded({extended:true}))
+app.use(cors())
 //Iniciando o banco de dados
 mongoose.connect(//conectando ao banco de dados mongo
     "mongodb+srv://fpdeploy:fooddeploy@teste-b8h6q.mongodb.net/test?retryWrites=true&w=majority",
