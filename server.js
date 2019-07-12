f41ed79f42c4a2163fa057a320a9ba31c1742992
Gabriel <44253377+Gabriel-ved@users.noreply.github.com>
2019-07-11 22:16:13 -0300

    corss

--- server.js
+++ server.js
@@ -3,17 +3,12 @@ const mongoose = require('mongoose');//mongodb framework
 const requireDir = require('require-dir');
 const cors = require('cors');
 
+app.use(cors())
 //Iniciando o app
 const app = express();//iniciando o framework
 app.use(express.json());//para o express aceitar post com json
 app.use(express.urlencoded({extended:true}))
-app.options("/*", function(req, res, next){
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  res.send(200);
-});
-//app.use(cors())
+
 //Iniciando o banco de dados
 mongoose.connect(//conectando ao banco de dados mongo
     "mongodb+srv://fpdeploy:fooddeploy@teste-b8h6q.mongodb.net/test?retryWrites=true&w=majority",
