b3a79fabff4747ed74574bbcdb41e9bf84fd568a
gabriel <fenexs@outlook.com>
2019-08-16 00:52:46 -0300

    dotenv 2

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv').config
+require('dotenv').config();
 const express = require('express');//backend framework for nodejs
 const mongoose = require('mongoose');//mongodb framework
 const requireDir = require('require-dir');
